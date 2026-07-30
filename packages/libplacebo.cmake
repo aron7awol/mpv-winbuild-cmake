69fd8c6951f3d20adaeaae25c019224ae9908d22
Aaron <aaronmedeiros@gmail.com>
2026-07-29 21:14:24 -0400

    Invalidate cached libplacebo when patches change

--- packages/libplacebo.cmake
+++ packages/libplacebo.cmake
@@ -1,5 +1,9 @@
 get_property(src_glad TARGET glad PROPERTY _EP_SOURCE_DIR)
 get_property(src_fast_float TARGET fast_float PROPERTY _EP_SOURCE_DIR)
+file(GLOB libplacebo_patches CONFIGURE_DEPENDS
+    "${CMAKE_CURRENT_LIST_DIR}/libplacebo-*.patch")
+list(SORT libplacebo_patches)
+
 ExternalProject_Add(libplacebo
     DEPENDS
         vulkan
@@ -14,7 +18,9 @@ ExternalProject_Add(libplacebo
     GIT_CLONE_FLAGS "--filter=tree:0"
     GIT_SUBMODULES ""
     UPDATE_COMMAND ""
-    PATCH_COMMAND ${EXEC} git am --3way ${CMAKE_CURRENT_SOURCE_DIR}/libplacebo-*.patch
+    PATCH_COMMAND bash -c "git am --abort 2>/dev/null || true"
+    COMMAND ${EXEC} git reset --hard origin/master
+    COMMAND ${EXEC} git am --3way ${libplacebo_patches}
     CONFIGURE_COMMAND ""
     COMMAND bash -c "rm -rf <SOURCE_DIR>/3rdparty/glad"
     COMMAND bash -c "rm -rf <SOURCE_DIR>/3rdparty/fast_float"
@@ -34,6 +40,18 @@ ExternalProject_Add(libplacebo
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+# ExternalProject does not track files consumed only by PATCH_COMMAND. Give
+# the patch step a file-level predecessor so adding or editing a patch makes
+# cached builds rerun patch -> configure -> build -> install for libplacebo.
+ExternalProject_Add_Step(libplacebo patchset-dependency
+    DEPENDEES update
+    DEPENDERS patch
+    DEPENDS ${libplacebo_patches}
+    INDEPENDENT TRUE
+    COMMAND ${CMAKE_COMMAND} -E echo
+        "libplacebo patch set changed; invalidating cached package stamps"
 )
 
 force_rebuild_git(libplacebo)
